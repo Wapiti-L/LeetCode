3ff11cdb1217253f0a1f12d2f410dbc8a1ef5266
wapiti <wapiti_l@163.com>
2021-04-08 20:42:05 +0800

    merge

--- src/simple/Solution.java
+++ src/simple/Solution.java
@@ -1,5 +1,6 @@
 package simple;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Stack;
 
@@ -304,6 +305,16 @@ public class Solution {
         }
 
     //合并两个有序数组
+    public void merge(int[] nums1, int m, int[] nums2, int n){
+        for (int i = 0; i < n; i ++){
+            nums1[m+i] = nums2[i];
+        }
+        Arrays.sort(nums1);
+    }
+
+
+
+
     public static void main(String[] args) {
         int x = 1;
 
