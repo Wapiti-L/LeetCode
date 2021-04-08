f71a12eab4c332ddc2cabfd1342477c15c037839
wapiti <wapiti_l@163.com>
2021-04-08 21:04:18 +0800

    maxDepth

--- src/simple/Solution.java
+++ src/simple/Solution.java
@@ -2,6 +2,7 @@ package simple;
 
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 /**
@@ -312,10 +313,35 @@ public class Solution {
         Arrays.sort(nums1);
     }
 
+    //二叉树的最大深度(Queue)
+    public int maxDepth(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        //定义队列，层次遍历
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int depth = 0;
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            while (size > 0){
+                TreeNode node = queue.poll();
+                if (node.left != null){
+                    queue.offer(node.left);
+                }
+                if (node.right != null){
+                    queue.offer(node.right);
+                }
+                size --;
+            }
+            depth ++;
+        }
+        return depth;
+    }
 
 
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         int x = 1;
 
         System.out.println( mySqrt(x));
