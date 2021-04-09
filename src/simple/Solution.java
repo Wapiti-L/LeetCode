d680c52c00a43d0a3690676250620b570e536764
wapiti <wapiti_l@163.com>
2021-04-09 21:25:26 +0800

    sortedArrayToBST

--- src/simple/Solution.java
+++ src/simple/Solution.java
@@ -340,6 +340,22 @@ public class Solution {
     }
 
 
+    //有序数组构造二叉树(递归)
+    public TreeNode sortedArrayToBST(int[] nums){
+        return dfs(nums,0,nums.length - 1);
+    }
+    private TreeNode dfs(int[] nums,int left,int right){
+        if (left > right){
+            return null;
+        }
+        int mid = (left + right) / 2 + left;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = dfs(nums,left,mid - 1);
+        root.right = dfs(nums,mid + 1, right);
+        return root;
+    }
+
+
 
         public static void main(String[] args) {
         int x = 1;
