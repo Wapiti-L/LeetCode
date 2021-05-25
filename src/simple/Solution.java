b247fa587b356fc22cec167d27f6426bb9610415
wapiti <wapiti_l@163.com>
2021-05-25 20:19:25 +0800

    inorderTraversal

--- src/simple/Solution.java
+++ src/simple/Solution.java
@@ -480,6 +480,26 @@ public class Solution {
     }
 
 
+    //二叉树中序遍历
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        inorder(root, res);
+        return res;
+    }
+
+    public void inorder(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, res);
+        res.add(root.val);
+        inorder(root.right, res);
+    }
+
+
+
+
+    
     public static void main(String[] args) {
         int x = 1;
 
