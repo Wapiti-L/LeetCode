65d897da0c489d5db528d85bd4ab46d7512da935
wapiti <wapiti_l@163.com>
2021-10-13 18:16:44 +0800

    addTwoNumbers

--- src/simple/Solution.java
+++ src/simple/Solution.java
@@ -508,7 +508,34 @@ public class Solution {
         return sb.reverse().toString();
     }
 
-
+    //两数相加
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2){
+        ListNode res = null, temp = null;
+        int carry = 0; //进位
+        while(l1 != null || l2 != null){
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;//取值 为空则用0代替
+            int sum = n1 + n2 + carry;
+            if(res == null){
+                res = temp = new ListNode(sum % 10);    //结果链表为空则从头赋值，
+            }else{
+                temp.next = new ListNode(sum % 10);     //结果不空，temp滑动
+                temp = temp.next;
+            }
+            carry = sum / 10;
+            if(l1 != null){
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                l2 = l2.next;
+            }
+        }
+        //如果还存在进位，需在末尾加上
+        if(carry != 0){
+            temp.next = new ListNode(carry);
+        }
+        return res;
+    }
 
     public static void main(String[] args) {
         int x = 1;
