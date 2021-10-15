0e8a4896449a88af22ac4f5f9b153f4a6e70aadb
wapiti <wapiti_l@163.com>
2021-10-15 21:20:13 +0800

    findNumberIn2DArray

--- src/simple/Solution.java
+++ src/simple/Solution.java
@@ -554,6 +554,22 @@ public class Solution {
         return -1;
     }
 
+    // 二维数组中的查找
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        int i = matrix.length - 1, j = 0;
+        while (i > 0 && j < matrix.length){
+            if (matrix[i][j] > target) {
+                i--;
+            }else if (matrix[i][j] < target){
+                j++;
+            }else {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
 
     public static void main(String[] args) {
         int x = 1;
